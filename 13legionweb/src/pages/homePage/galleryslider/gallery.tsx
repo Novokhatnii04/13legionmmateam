fc8ae2143656b2e9621be5bffbf9fa4446881461
Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
2024-05-21 15:39:53 +0300

    added slider to project

--- 13legionweb/src/pages/homePage/galleryslider/gallery.tsx
+++ 13legionweb/src/pages/homePage/galleryslider/gallery.tsx
@@ -1,13 +1,41 @@
-import styles from './gallery.module.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-
-// Import Swiper styles
+import styles from './gallery.module.css'; // Adjust the path to your actual CSS file
+import sliderImg1 from "../../../assets/images/slider/imageclub1.png";
+import sliderImg2 from "../../../assets/images/slider/imageclub2.png";
 
 const Gallery = () => {
   return (
-    <div className={styles.gallery}>
-   
-    </div>
+    <section className={styles.gallery}>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>
+          <div
+            style={{ backgroundImage: `url(${sliderImg1})` }}
+            className={styles.gallery_image}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            style={{ backgroundImage: `url(${sliderImg2})` }}
+            className={styles.gallery_image}
+          />
+        </SwiperSlide>
+        {/* Add more SwiperSlides as needed */}
+      </Swiper>
+    </section>
   );
 };
 
